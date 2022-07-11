88df5c74f36b062aa0026732d7ba92fdbf5218b7
陈世强 <11199371+chenshiqiangCSQ@user.noreply.gitee.com>
2022-07-11 19:09:23 +0800

    定时器

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //创建调度器
+            ISchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+
+            //创建JobDetail
+            var jobdetail = JobBuilder.Create<MyJob>().WithIdentity("myjob").Build();
+
+            //键值对
+            IDictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("a", Guid.NewGuid());
+            data.Add("b", Guid.NewGuid());
+
+            var jobdata = new JobDataMap(data);
+
+            var job = JobBuilder.Create<MyJob>().SetJobData(jobdata).WithIdentity(new JobKey("每三秒触发")).Build();
+
+            //创建触发器
+            var trigger = TriggerBuilder.Create().WithSimpleSchedule(t =>
+            {
+                t.WithInterval(TimeSpan.FromSeconds(3)).WithRepeatCount(10);
+            }).Build();
+
+            //通过调度器调度作业执行
+            await scheduler.ScheduleJob(job, trigger);
+            await scheduler.Start();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MyJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine(context.JobDetail.JobDataMap.Get("a"));
+            Console.WriteLine(context.JobDetail.Key.Name);
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
